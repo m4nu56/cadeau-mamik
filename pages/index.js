45860b993295bb52b7e0a8609c58eaa3e9680666
Emmanuel Balpe <m4nu56@gmail.com>
2020-12-22 14:11:54 +0100

    :bug: defualt headline couture

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { GiftContainer } from '../components/GiftContainer'
 
 // noinspection JSUnusedGlobalSymbols
-const Home = ({ recipient = 'Mamik', header = '3 séances de', headline = 'dancing' }) =>
+const Home = ({ recipient = 'Mamik', header = '3 séances de', headline = 'couture' }) =>
   <GiftContainer recipient={recipient} header={header} headline={headline}/>
 
 Home.getInitialProps = ({ query }) => query
